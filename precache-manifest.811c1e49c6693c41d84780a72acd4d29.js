0b7529f4112dc98664015f0ef0181bfec68ffb81
Odis Barnett <kingo.iv@gmail.com>
2020-10-15 19:52:00 -0500

    Updates

---
+++ precache-manifest.811c1e49c6693c41d84780a72acd4d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28d2af308b7230d401a7240896c1c5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92aa55c8cf8f1868ed95",
+    "url": "/static/css/main.9f7ec82d.chunk.css"
+  },
+  {
+    "revision": "dd72cda5089982df96b1",
+    "url": "/static/js/2.0a0ee5fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0a0ee5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92aa55c8cf8f1868ed95",
+    "url": "/static/js/main.5c333dec.chunk.js"
+  },
+  {
+    "revision": "5fe3c02f00608b047441",
+    "url": "/static/js/runtime-main.29a939eb.js"
+  }
+]);
